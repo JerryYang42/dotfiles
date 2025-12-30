6c1c85e7acba17d80fc1d30be1fa3da43f3d23c9
Jerry Yang <j.yang5@elsevier.com>
2025-12-30 20:20:01 +0100

    add npkill global npm package

---
+++ install-global-npm-packages.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+# Install global npm packages                                                {{{1
+# ==============================================================================
+npm install -g npkill
